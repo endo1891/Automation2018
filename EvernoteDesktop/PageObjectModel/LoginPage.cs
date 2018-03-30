5bddf700273f6722962f1fc9d2632c5cbd7fec9b
Enda Carroll <endacarroll2008@gmail.com>
2018-03-31 00:47:04 +0200

    *

---
+++ EvernoteDesktop/PageObjectModel/LoginPage.cs
@@ -0,0 +1,34 @@
+﻿using EvernoteCommon;
+using NLog;
+using System;
+
+namespace EvernoteDesktop.PageObjectModel
+{
+    public class LoginPage
+    {
+        private readonly TestingProfile _testingProfile;
+        private readonly TestContext _testContext;
+        public ITestFrameworkConfiguration _config;
+        private readonly Logger _logger = LogManager.GetCurrentClassLogger();
+
+        public LoginPage(TestContext testContext, TestingProfile testProfile, ITestFrameworkConfiguration config)
+        {
+            _testingProfile = testProfile;
+            _testContext = testContext;
+            _config = config;
+
+            Initialize();
+        }
+
+        public void Initialize()
+        {
+            Username = new WebElement(desktoplabels.Username, _testingProfile.WebDriver);
+            LoginButton = new WebElement(desktoplabels.LoginButton, _testingProfile.WebDriver);
+            Password = new WebElement(desktoplabels.Password, _testingProfile.WebDriver);
+        }
+
+        public WebElement Username { get; set; }
+        public WebElement LoginButton { get; set; }
+        public WebElement Password { get; set; }
+    }
+}
---
+++ EvernoteDesktop/TestContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EvernoteDesktop
+{
+
+    public class TestContext
+    {
+        public bool IsLoggedIn { get; set; }
+        string CurrentUsername { get; set; }
+        public string CurrentPassword { get; set; }
+        bool IsMenuOpen { get; set; }
+        Uri PasswordReset { get; set; }
+
+        public TestContext()
+        {
+            IsLoggedIn = false;
+        }
+
+        public void SetLoggedIn()
+        {
+            IsLoggedIn = true;
+        }
+    }
+}
