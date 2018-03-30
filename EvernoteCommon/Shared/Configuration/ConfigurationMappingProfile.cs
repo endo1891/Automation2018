337f490defaf6dfa094d7e7dfe6fbbb5f003052f
Enda Carroll <endacarroll2008@gmail.com>
2018-03-31 00:35:05 +0200

    *

---
+++ EvernoteCommon/Shared/Configuration/ConfigurationMappingProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace EvernoteCommon.Shared.Configuration
+{
+    public class ConfigurationMappingProfile : Profile
+    {
+        protected override void Configure()
+        {
+            CreateMap<IApplicationConfigurationSection, ApplicationConfiguration>();
+        }
+    }
+}
